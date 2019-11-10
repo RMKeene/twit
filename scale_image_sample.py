1c3a6228cd94ff8d2637f88609c3bdfd3b34640e
Richard Keene <10094570+RMKeene@users.noreply.github.com>
2019-11-10 14:20:10 -0700

    Image test on dog. 2.9 sec per twit in pure python. 1.9 ms. per scale C code.

--- scale_image_sample.py
+++ scale_image_sample.py
@@ -3,23 +3,36 @@ import twit
 import twitc
 from PIL import Image
 import matplotlib.pyplot as plt
+import time
 
 def scale_image():
     """
     Test code and example to scale an image using Twit.
     """
-
+    print("SCALE IMAGE - Pure Python")
     im = Image.open(r"K:\twit\dog1.jpg")
     plt.imshow(im, cmap='Greys_r')
     plt.show()
     pass
-    np_im = np.array(im)
+    np_im = np.array(im, dtype=np.float64)
  
     # 225 x 225 x 3
     print("Tensor shape: " + str(np_im.shape))
+    start = time.time()
 
-    dest = np.zeros((100, 200, 3))
-    twit.tensor_transfer(np_im, dest, preclear=False, weight_range=(0.0, 1.0), weight_axis=1)
+    dest = np.zeros((100, 200, 3), dtype=np.float64)
+
+    # twitc.make_and_apply_twit(3, np.array([0, np_im.shape[0] - 1, 0, np_im.shape[1] - 1, 0, np_im.shape[2] - 1, 0, 99, 0, 199, 0, 2], dtype=np.int64),  np.array([0.0, 1.0, 1.0, 1.0, 1.0, 1.0], dtype=np.float64), np_im, dest, 0)
+    twt = twit.compute_twit_multi_dimension(3, np.array([0, np_im.shape[0] - 1, 0, 99, 0, np_im.shape[1] - 1, 0, 199, 0, np_im.shape[2] - 1, 0, 2], dtype=np.int64),  np.array([0.0, 1.0, 0.0, 1.0, 1.0, 1.0], dtype=np.float64))
+    # t = twitc.unpack_twit_multi_axis(twt)
+    N = 3
+    pc = 1
+    print("Start apply twit loop %d iterations with preclear %d" % (N, pc))
+    for i in range(N):
+        twit.apply_twit(twt, np_im, dest, 1)
+    end = time.time()
+    print("Time elapsed: %d sec." % (end - start))
+    print("End apply twit loop")
     new_im = Image.fromarray(dest.astype(np.uint8))
 
     plt.imshow(new_im, cmap='Greys_r')
@@ -30,7 +43,7 @@ def scale_image_C_code():
     """
     Test code and example to scale an image using Twit.
     """
-
+    print("SCALE IMAGE - C Code")
     im = Image.open(r"K:\twit\dog1.jpg")
     plt.imshow(im, cmap='Greys_r')
     plt.show()
@@ -39,17 +52,19 @@ def scale_image_C_code():
  
     # 225 x 225 x 3
     print("Tensor shape: " + str(np_im.shape))
-
+    start = time.time()
     dest = np.zeros((100, 200, 3), dtype=np.float64)
 
     # twitc.make_and_apply_twit(3, np.array([0, np_im.shape[0] - 1, 0, np_im.shape[1] - 1, 0, np_im.shape[2] - 1, 0, 99, 0, 199, 0, 2], dtype=np.int64),  np.array([0.0, 1.0, 1.0, 1.0, 1.0, 1.0], dtype=np.float64), np_im, dest, 0)
     twt = twitc.compute_twit_multi_dimension(3, np.array([0, np_im.shape[0] - 1, 0, 99, 0, np_im.shape[1] - 1, 0, 199, 0, np_im.shape[2] - 1, 0, 2], dtype=np.int64),  np.array([0.0, 1.0, 0.0, 1.0, 1.0, 1.0], dtype=np.float64))
     # t = twitc.unpack_twit_multi_axis(twt)
-    N = 1
+    N = 10000
     pc = 1
     print("Start apply twit loop %d iterations with preclear %d" % (N, pc))
     for i in range(N):
         twitc.apply_twit(twt, np_im, dest, 1)
+    end = time.time()
+    print("Time elapsed: %d sec." % (end - start))
     print("End apply twit loop")
     new_im = Image.fromarray(dest.astype(np.uint8))
 
@@ -60,5 +75,7 @@ def scale_image_C_code():
  
 if __name__ == '__main__':
     print("Scale Image with twit sample program.")
+    
     scale_image()
-    #scale_image_C_code()
+    
+    scale_image_C_code()
